12115848352ed62dd6d08f473799fa1d8079febf
Daren <darenminarolli@gmail.com>
2024-01-31 17:27:19 +0100

    fix

--- app/[...slug]/page.js
+++ app/[...slug]/page.js
@@ -4,18 +4,18 @@ import Services from "@/components/Services";
 import Works from "@/components/Works";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
-import { fetchUsers, filterUser } from "@/services/fetchUsers";
+// import { fetchUsers, filterUser } from "@/services/fetchUsers";
 
 export default  async function Home({params}) {
 
-  const { dentists } = await fetchUsers()
-  console.log(dentists, 'we are here')
+  // const { dentists } = await fetchUsers()
+  // console.log(dentists, 'we are here')
 
-   const filteredUser= await filterUser(params.slug)
-   const users = filteredUser[0]
+  //  const filteredUser= await filterUser(params.slug)
+  //  const users = filteredUser[0]
   return (
     <>
-    <Nav name={users?.fullName}/>
+    <Nav />
     <main className="w-full md:w-[1250px] m-auto mt-20 flex min-h-screen flex-col items-center justify-between gap-10 p-4 md:p-24 ">
      <About/>
      <Services/>
--- services/fetchUsers.js
+++ services/fetchUsers.js
@@ -1,33 +1,33 @@
-export async function fetchUsers() {
-    try {
-      const res = await fetch('http://localhost:3000/api/Dentists');
+// export async function fetchUsers() {
+//     try {
+//       const res = await fetch('http://localhost:3000/api/Dentists');
   
-      if (!res.ok) {
-        // If the initial fetch is not successful, try fetching from the production URL
-        const prodRes = await fetch('https://dentist-blog.vercel.app/api/Dentists');
-        if (!prodRes.ok) {
-          throw new Error('Failed to fetch user from production API');
-        }
-        return prodRes.json();
-      }
+//       if (!res.ok) {
+//         // If the initial fetch is not successful, try fetching from the production URL
+//         const prodRes = await fetch('https://dentist-blog.vercel.app/api/Dentists');
+//         if (!prodRes.ok) {
+//           throw new Error('Failed to fetch user from production API');
+//         }
+//         return prodRes.json();
+//       }
   
-      return res.json();
-    } catch (error) {
-      console.error('Failed to fetch user', error);
-      throw new Error('Failed to fetch user');
-    }
-  }
+//       return res.json();
+//     } catch (error) {
+//       console.error('Failed to fetch user', error);
+//       throw new Error('Failed to fetch user');
+//     }
+//   }
   
 
-export async function filterUser (params){
-    try{
-        const {dentists} = await fetchUsers()
-        const userparam= params.join(', ')
-        const filteredUser =  dentists.filter((dentist)=> dentist.fullName.toLowerCase().replace(/\s/g, "") === userparam);
-       console.log(filteredUser, 'here we are ok')
-    //    console.log( userparam === 'aleksvisha', 'hi')
-      return filteredUser
-    }catch(e){
-     console.log('Failed to filter user', e)
-    }
-}
+// export async function filterUser (params){
+//     try{
+//         const {dentists} = await fetchUsers()
+//         const userparam= params.join(', ')
+//         const filteredUser =  dentists.filter((dentist)=> dentist.fullName.toLowerCase().replace(/\s/g, "") === userparam);
+//        console.log(filteredUser, 'here we are ok')
+//     //    console.log( userparam === 'aleksvisha', 'hi')
+//       return filteredUser
+//     }catch(e){
+//      console.log('Failed to filter user', e)
+//     }
+// }
